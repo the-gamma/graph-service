c779ef9cdee4229d70c60708fb055323661900ac
Tomas Petricek <tomas@tomasp.net>
2019-06-20 13:58:36 +0100

    Revert password

--- neo4j.js
+++ neo4j.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver').v1;
-var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "garfield"));
+var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "azerty"));
 const session = driver.session();
 const fs = require('fs')
 
